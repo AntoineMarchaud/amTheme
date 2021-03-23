3436bd587d0d9da4ba73aaeaa121e0d93a974f61
Antoine Marchaud <tonio2835@gmail.com>
2021-03-23 15:46:04 +0100

    tweaks

--- app/src/main/java/com/amarchaud/amTheme/MainActivity.kt
+++ app/src/main/java/com/amarchaud/amTheme/MainActivity.kt
@@ -69,10 +69,10 @@ class MainActivity : AppCompatActivity() {
                             // content doesn't resize when the system bars hide and show.
                             //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            //or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             // Hide the nav bar and status bar
                             or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            //or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
                     )
         }
     }
