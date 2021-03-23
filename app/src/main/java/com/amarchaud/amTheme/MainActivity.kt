8a20c41010f884fced960604e493b335bd99c613
Antoine Marchaud <tonio2835@gmail.com>
2021-03-23 15:41:48 +0100

    tweaks

--- app/src/main/java/com/amarchaud/amTheme/MainActivity.kt
+++ app/src/main/java/com/amarchaud/amTheme/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.amarchaud.amTheme
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.doOnLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -42,50 +42,39 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
             setSupportActionBar(toolbar)
-            supportActionBar?.setDisplayShowTitleEnabled(false) // todo does not work !
             toolbar.setupWithNavController(navController, appBarConfiguration)
             bottomNav.setupWithNavController(navController)
         }
 
-        /*
         // remove bottom nav bar (home + back)
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) { // android 11
             window.insetsController?.let {
                 // Default behavior is that if navigation bar is hidden, the system will "steal" touches
                 // and show it again upon user's touch. We just want the user to be able to show the
                 // navigation bar by swipe, touches are handled by custom code -> change system bar behavior.
                 // Alternative to deprecated SYSTEM_UI_FLAG_IMMERSIVE.
                 it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                // make navigation bar translucent (alternative to deprecated
-                // WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-                // - do this already in hideSystemUI() so that the bar
-                // is translucent if user swipes it up
-                window.navigationBarColor = getColor(R.color.seaweed_green)
-                // Finally, hide the system bars, alternative to View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                // and SYSTEM_UI_FLAG_FULLSCREEN.
-                it.hide(WindowInsets.Type.systemBars())
+                // Finally, hide the nav bars, alternative to View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                it.hide(WindowInsets.Type.navigationBars())
             }
         } else {
-
             // Enables regular immersive mode.
-            // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-            // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            /*
+            // For "lean back" mode : remove first parameter : one click, show statusbar
+            // SYSTEM_UI_FLAG_IMMERSIVE : plus d'actionBar, swipe pour retrouver statusBar + navBar
+            // SYSTEM_UI_FLAG_IMMERSIVE_STICKY : idem, mais statusBar + navBar disparait tout seul après quelques secondes
             window.decorView.systemUiVisibility = (
-                    // Do not let system steal touches for showing the navigation bar
-                    View.SYSTEM_UI_FLAG_IMMERSIVE
-                            // Hide the nav bar
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            // Set the content to appear under the system bars so that the
+                            // content doesn't resize when the system bars hide and show.
+                            //or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            //or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            // Hide the nav bar and status bar
                             or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            // Keep the app content behind the bars even if user swipes them up
-                            /*or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN*/)
-            // make navbar translucent - do this already in hideSystemUI() so that the bar
-            // is translucent if user swipes it up*/
-
-            //window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+                            //or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    )
         }
-         */
-
     }
 
     fun changeToolbarTitle(title: String) {
